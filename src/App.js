0f47710ce62a8ba7a683197ffa618aa315394c98
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-08 15:58:11 +0900

    로그인 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
+import LoginScreen from './component/LoginScreen';
 
 class App extends Component {
   render() {
     return (
-      <div>hello!</div>
+      <LoginScreen/>
     );
   }
 }
---
+++ src/component/LoginScreen.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Icon,
+  Segment,
+  Grid,
+  Header,
+} from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const FullHeightGrid = styled(Grid)`
+  height: 620px;
+  /* background-color: #f03e3e; */
+`;
+
+export default class LoginScreen extends Component {
+  static defaultProps ={
+    onGoogleLogin: () => {},
+  }
+
+  render() {
+    return (
+      <FullHeightGrid centered verticalAlign="middle">
+        <Grid.Column style={{ width: '320px'}}>
+          <Segment textAlign="center" vertical>
+            <Header as="h1" textAlign="center">PPOM</Header>
+            <p>시간과 함께 일하는 습관</p>
+            <Button color='google plus'>
+              <Icon name='google plus' />Google 로그인
+            </Button>
+          </Segment>
+        </Grid.Column>
+      </FullHeightGrid>
+    )
+  }
+}
