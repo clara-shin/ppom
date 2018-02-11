abf686e73ccc4dc4f84b4c2dff0de20cccaaf083
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-12 01:04:34 +0900

    불필요한 파일 삭제

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import * as firebase from 'firebase';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
