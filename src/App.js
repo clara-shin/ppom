414d2409a9d7b41e6ab5370e3d6fed1bca8a8075
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-14 18:35:24 +0900

    메인스크린에 헤더 및 목표리스트 추가 및 생성 #3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import LoginScreenContainer from './containers/LoginScreenContainer';
-import GoalList from './components/GoalList';
+import MainScreen from './components/MainScreen';
 import withAuth from './hocs/withAuth';
 import rootReducer from './ducks';
 
@@ -19,7 +19,7 @@ class App extends Component {
           <div>
             <Route path="/" exact component={Home} />
             <Route path="/login" component={LoginScreenContainer} />
-            <Route path="/list" component={GoalList} />
+            <Route path="/list" component={MainScreen} />
           </div>
         </BrowserRouter>
       </Provider>
