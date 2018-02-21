6d8bc3a75ec78d53b2963b7a81bc2c4c01ff85cd
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-21 17:19:06 +0900

    eslint error fix (7) stories/ 컴포넌트
    - 간격 및 세미콜론 eslint error fix 완료
    - 사용하지 않는 파일은 import 삭제함

--- stories/LoginScreen.js
+++ stories/LoginScreen.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import { Button, Welcome } from '@storybook/react/demo';
 
 import LoginScreen from '../src/components/LoginScreen';
 
@@ -12,4 +9,4 @@ import LoginScreen from '../src/components/LoginScreen';
 storiesOf('LoginScreen', module)
   .add('default', () => (
     <LoginScreen onGoogleLogin={action('onGoogleLogin')} />
-));
+  ));
