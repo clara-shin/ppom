cd5d9128c4ebfdc99485f79c9960f0c3004ef571
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-21 17:14:33 +0900

    eslint error fix (3) LoginScreen 컴포넌트
    - 간격 및 세미콜론 eslint error fix 완료

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -8,7 +8,7 @@ import {
 } from 'semantic-ui-react';
 import styled from 'styled-components';
 
-const FullHeightGrid = styled(Grid) `
+const FullHeightGrid = styled(Grid)`
   height: 620px;
   /* background-color: #f03e3e; */
 `;
@@ -29,12 +29,12 @@ export default class LoginScreen extends Component {
           <Segment textAlign="center" vertical>
             <Header as="h1" textAlign="center">PPOM</Header>
             <p>시간과 함께 일하는 습관</p>
-            <Button color='google plus' onClick={this.handleGoogleButtonClick}>
-              <Icon name='google plus' />Google 로그인
+            <Button color="google plus" onClick={this.handleGoogleButtonClick}>
+              <Icon name="google plus" />Google 로그인
             </Button>
           </Segment>
         </Grid.Column>
       </FullHeightGrid>
-    )
+    );
   }
 }
