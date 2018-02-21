2a7efd11f140221444aa69e81e181cf3322071ca
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-21 17:18:21 +0900

    eslint error fix (6) LoginScreenContainer 컴포넌트
    - import 우선순위별 위치 변경. firebase와 react-router-dom을 위로 두고 LoginScreen을 밑으로 둠
    - 간격 및 세미콜론 eslint error fix 완료
    - eslint 규칙에 따라 render() 부분의 if else구문은 if 와 return 구문으로 변경됨

--- src/containers/LoginScreenContainer.js
+++ src/containers/LoginScreenContainer.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import LoginScreen from '../components/LoginScreen';
 import * as firebase from 'firebase';
 import { Redirect } from 'react-router-dom';
+import LoginScreen from '../components/LoginScreen';
 
 export default class LoginScreenContainer extends Component {
   state = {
-    redirectToList: false
+    redirectToList: false,
   }
 
   handleGoogleLogin = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     await firebase.auth().signInWithRedirect(provider);
     this.setState({
-      redirectToList: true
+      redirectToList: true,
     });
   }
 
@@ -20,11 +20,10 @@ export default class LoginScreenContainer extends Component {
     if (this.state.redirectToList) {
       return (
         <Redirect to="/list" />
-      )
-    } else {
-      return (
-        <LoginScreen onGoogleLogin={this.handleGoogleLogin} />
       );
     }
+    return (
+      <LoginScreen onGoogleLogin={this.handleGoogleLogin} />
+    );
   }
 }
