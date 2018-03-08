ce618dd55fccc6f172409093630077f6812f01e4
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-03-08 21:11:24 +0900

    랜딩페이지 생성

---
+++ src/components/Landing.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Image } from 'semantic-ui-react';
+import LogoImg from './Landing/img/tomato_on.png';
+
+const Wrap = styled.div`
+  position:relative;
+  width:100vw;
+  height:100vh;
+  background-color: #f03e3e;
+`;
+
+const LandingHeader = styled.header`
+  height: 95vh;
+`;
+
+const LinkWrap = styled.div`
+  position: fixed !important;
+  left: 50%;
+  bottom: 20%;
+  display: flex;
+  justify-content: center;
+  transform: translateX(-50%);
+`;
+const HeaderLogo = styled.div`
+  padding: 40% 0 0;
+`;
+const HeaderText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
+const Button = styled.button`
+  display: inline-block;
+`;
+const Link = Button.withComponent('a');
+
+const HeaderLink = styled(Link)`
+  font-size:1.2em !important;
+`;
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <Wrap>
+        <LandingHeader>
+          <HeaderLogo>
+            <Image src={LogoImg} alt="Logo" size="tiny" centered />
+          </HeaderLogo>
+          <HeaderText>
+            <h1 className="heading-primary">
+              <span className="heading-primary--main">PPOM</span>
+              <span className="heading-primary--sub">pomodoro timer</span>
+            </h1>
+          </HeaderText>
+
+          <LinkWrap>
+            <HeaderLink className="btn btn--white btn--animated">시작하기</HeaderLink>
+          </LinkWrap>
+
+        </LandingHeader>
+      </Wrap>
+    );
+  }
+}
